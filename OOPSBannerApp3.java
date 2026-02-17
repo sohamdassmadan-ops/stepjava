276972e739db1e57c97b4e4a924f1d1cbc8e3aa5
sohamdassmadan-ops <sohamdassmadan@gmail.com>
2026-02-17 09:17:09 +0530

    SECOND COMMIT

---
+++ OOPSBannerApp3.java
@@ -0,0 +1,16 @@
+public class OOPSBannerApp3 {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join("\n",
+                " *****    *****   ******   *****  ",
+                "*     *  *     *  *     *  *     * ",
+                "*     *  *     *  *     *  *     * ",
+                "*     *  *     *  ******   *****  ",
+                "*     *  *     *  *        *      ",
+                "*     *  *     *  *        *      ",
+                " *****    *****   *        *      "
+        ));
+
+    }
+}
